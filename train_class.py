be78433bb18ad42e3e47fcf904313a9b3b95be72
aliciaviernes <feggarodymeni@gmail.com>
2021-07-15 14:38:40 +0200

    First commit - Baseline modules.

---
+++ train_class.py
@@ -0,0 +1,128 @@
+"""
+Classification module. Structure as per SBERT suggestion.
+Addition of iteration loops and stats.
+"""
+from torch.utils.data import DataLoader
+import math
+from sentence_transformers import LoggingHandler, util 
+import logging
+from datetime import datetime
+import time
+import os
+import baseline.util as util
+import argparse
+import json
+import torch  # needed for classification - save state dict
+
+from baseline.modelling import modifiedSentenceTransformer
+from baseline.loss import modifiedSoftmaxLoss
+
+from baseline.evaluate import LabelAccuracyEvaluator
+
+# from EDA.mod_augment import *
+# from TP.tp_train import *
+# from WordEmb.we_train import *
+
+parser = argparse.ArgumentParser(description="Training hyperparameters")
+parser.add_argument('--batch_size', type=int, default=256, required=False, help="Train batch size")
+parser.add_argument('--num_epochs', type=int, default=4, required=False, help="Number of epochs")
+parser.add_argument('--eval_loops', type=int, default=8, required=False, help="Evaluation loops in each epoch")
+parser.add_argument('--warmup_percent', type=float, default=0.1, required=False, help="Percent of warming steps (0.1 for 10%)")
+parser.add_argument('--num_iterations', '-i', type=int, default=20, required=False, help="Number of iterations")
+aug_group = parser.add_mutually_exclusive_group()
+aug_group.add_argument('--eda', '-e', action='store_true', help='Easy Data Augmentation')
+aug_group.add_argument('--tp', '-t', action='store_true', help='TransPlacement')
+aug_group.add_argument('--we', '-w', action='store_true', help='Word Embedding Synonym Replacement')
+args = parser.parse_args()
+
+num_labels = 2; dataset = 'MSRP'
+dataset_path = 'data/datasets/MSRP/'
+
+# Specification of BERT model and save path.
+model_name = 'distilbert-base-multilingual-cased'  # change to multilingual
+model_save_path = f'output/training_class_{dataset}_'+model_name.replace("/", "-")+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+#### Just some code to print debug information to stdout
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+#### /print debug information to stdout
+
+starttime = time.time()
+
+
+iter_outer = dict() # one dictionary for all iterations
+
+for i in range(args.num_iterations):
+
+    if i < 10:
+        i = str(i).zfill(2)
+
+    train_samples = util.msrp_base(f'{dataset_path}msr_paraphrase_train-new.csv')  # Important
+    dev_samples = util.msrp_base(f'{dataset_path}msr_paraphrase_dev.csv')
+    test_samples = util.msrp_base(f'{dataset_path}msr_paraphrase_test.csv')     
+
+    logging.info(f"ITERATION {i}")
+
+    model = modifiedSentenceTransformer(model_name)
+
+    iter_inner = {'epoch':[], 'global_steps':[]}  # inner dictionary for each iteration
+
+    logging.info(f"Augment {dataset} dataset - Train") 
+    
+    # if args.eda:
+        # train_samples = eda_train(train=train_samples, num_aug=3)
+    # elif args.tp:
+        # train_samples = tp_train(corpus='msrp', train=train_samples, nr_aug=3)
+    # elif args.we:
+        # train_samples = we_train(train=train_samples, nr_aug=3)
+    # else:
+        # train_samples = util.zeroaugment(train_samples=train_samples)
+
+
+    logging.info(f"Load {dataset} dataset, initialize loss and evaluators.")
+    train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=args.batch_size)
+    len_datapoints = len(train_dataloader)
+    dev_dataloader = DataLoader(dev_samples, shuffle=False, batch_size=args.batch_size)
+    test_dataloader = DataLoader(test_samples, shuffle=False, batch_size=args.batch_size)
+
+    # Loss & evaluators
+    train_loss = modifiedSoftmaxLoss(model=model, sentence_embedding_dimension=model.get_sentence_embedding_dimension(), num_labels=num_labels)
+    dev_evaluator = LabelAccuracyEvaluator(dev_dataloader, softmax_model=train_loss, name=f'{dataset}-dev')
+    
+    # Configure warm-up steps
+    warmup_steps = math.ceil(len(train_dataloader) * args.num_epochs * args.warmup_percent)
+    logging.info("Warmup-steps: {}".format(warmup_steps))
+
+    # Train the model
+    model.fit(train_objectives=[(train_dataloader, train_loss)],
+            evaluator=dev_evaluator,
+            epochs=args.num_epochs,
+            evaluation_steps=len_datapoints // args.eval_loops + 1,
+            warmup_steps=warmup_steps,
+            output_path=model_save_path,
+            iter_dict=iter_inner,
+            iteration=i)
+    
+    # Needed for Class: Load the model incl. loss.
+    model = modifiedSentenceTransformer(model_save_path)
+    bestloss = modifiedSoftmaxLoss(model=model, sentence_embedding_dimension=model.get_sentence_embedding_dimension(), num_labels=num_labels)
+    bestloss.classifier.load_state_dict(torch.load(model_save_path + '/bestmodel_classifier.pth'))
+    test_evaluator = LabelAccuracyEvaluator(test_dataloader, softmax_model=bestloss, name=f'{dataset}-test')
+    test_evaluator(model, output_path=model_save_path, iter_dict=iter_inner, i=i)
+
+    iter_inner['epoch'].remove(-1)
+    iter_inner['global_steps'].remove(-1)
+    iter_outer[int(i)] = iter_inner
+
+outputfile = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+with open(f'{dataset}_{outputfile}.json', 'w') as f:
+    json.dump(iter_outer, f, indent=2)
+util.sum_best_dev_class(f'{dataset}_{outputfile}')
+util.add_statistics(f'{dataset}_{outputfile}.csv')
+    
+endtime = time.time()  
+
+elapsed = endtime - starttime
+logging.info(f"Time elapsed: {util.hours_minutes_seconds(elapsed)}")
---
+++ train_regr.py
@@ -0,0 +1,138 @@
+"""
+Regression module. Structure as per SBERT suggestion.
+Addition of iteration loops and stats.
+"""
+from torch.utils.data import DataLoader
+import math
+from sentence_transformers import LoggingHandler
+import logging
+from datetime import datetime
+import time
+import os
+import baseline.util as util
+import argparse
+import json
+
+from baseline.modelling import modifiedSentenceTransformer
+from baseline.loss import modifiedCosineSimilarityLoss
+
+from baseline.evaluate import EmbeddingSimilarityEvaluator
+
+# from EDA.mod_augment import *
+# from TP.tp_train import *
+# from WordEmb.we_train import *
+
+parser = argparse.ArgumentParser(description="Training hyperparameters")
+parser.add_argument('--batch_size', type=int, default=256, required=False, help="Train batch size")
+parser.add_argument('--num_epochs', type=int, default=4, required=False, help="Number of epochs")
+parser.add_argument('--eval_loops', type=int, default=8, required=False, help="Evaluation loops in each epoch")
+parser.add_argument('--warmup_percent', type=float, default=0.1, required=False, help="Percent of warming steps (0.1 for 10%)")
+parser.add_argument('--num_iterations', '-i', type=int, default=20, required=False, help="Number of iterations")
+datagroup = parser.add_mutually_exclusive_group()
+datagroup.add_argument('--stsb', action='store_true', help='STSb dataset')  
+datagroup.add_argument('--sick', action='store_true', help='SICK dataset')
+aug_group = parser.add_mutually_exclusive_group()
+aug_group.add_argument('--eda', '-e', action='store_true', help='Easy Data Augmentation')
+aug_group.add_argument('--tp', '-t', action='store_true', help='TransPlacement')
+aug_group.add_argument('--we', '-w', action='store_true', help='Word Embedding Synonym Replacement')
+args = parser.parse_args()
+
+if args.stsb:
+    dataset = 'stsb'
+    dataset_path = 'data/datasets/stsbenchmark.tsv.gz'
+elif args.sick:
+    dataset = 'sick'
+    dataset_path = 'data/datasets/SICK/SICK_annotated.csv'
+else:
+    print("Please choose a dataset [--stsb|-sick]")
+
+
+# Specification of BERT model and save path.
+model_name = 'distilbert-base-multilingual-cased' 
+model_save_path = f'output/training_regr_{dataset}_'+model_name.replace("/", "-")+'-'+datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+
+
+#### Just some code to print debug information to stdout
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+
+starttime = time.time()
+
+iter_outer = dict()  # one dictionary for all iterations
+
+for i in range(args.num_iterations):
+
+    # NOTE I don't like it
+    if i < 10:
+        i = str(i).zfill(2) 
+
+    if dataset == 'stsb':  # Loading of pre-split data
+        train_samples, dev_samples, test_samples = util.split_sts(dataset_path)
+    elif dataset == 'sick':
+        train_samples, dev_samples, test_samples = util.split_sick(dataset_path)
+
+    logging.info(f"ITERATION {i}")  # TODO?
+
+    model = modifiedSentenceTransformer(model_name)
+    
+    iter_inner = {'epoch':[], 'global_steps':[]}  # inner dictionary for each iteration    
+          
+    logging.info(f"Augment {dataset} dataset - Train")
+  
+    # if args.eda:
+        # train_samples = eda_train(train=train_samples, num_aug=3)
+    # elif args.tp:
+        # train_samples = tp_train(corpus=dataset, train=train_samples, nr_aug=3)
+    # elif args.we:
+        # train_samples = we_train(train=train_samples, nr_aug=3)
+    # else:
+        # train_samples = util.zeroaugment(train_samples=train_samples)
+
+    if len(train_samples) % 64 == 1:
+        del train_samples[-1]
+    logging.info(f"In this loop there are {len(train_samples)} train samples.")
+    
+    logging.info(f"Load {dataset} dataset, initialize loss and evaluators.")
+    train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=args.batch_size)
+    evaluation_steps = calculate_eval_steps(len(train_dataloader), args.eval_loops)
+    logging.info(f"Evaluation steps: {evaluation_steps}")
+    
+    # Loss & evaluators
+    train_loss = modifiedCosineSimilarityLoss(model=model)
+    dev_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, name=f'{dataset}-dev')
+    test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_samples, name=f'{dataset}-test')
+    
+    # Configure warm-up steps
+    warmup_steps = math.ceil(len(train_dataloader) * args.num_epochs * args.warmup_percent)
+    logging.info("Warmup-steps: {}".format(warmup_steps))
+
+    # Train the model
+    model.fit(train_objectives=[(train_dataloader, train_loss)],
+            evaluator=dev_evaluator,
+            epochs=args.num_epochs,
+            evaluation_steps=evaluation_steps,
+            warmup_steps=warmup_steps,
+            output_path=model_save_path,
+            iter_dict=iter_inner,
+            iteration=i)
+
+    model = modifiedSentenceTransformer(model_save_path)
+    test_evaluator(model, output_path=model_save_path, iter_dict=iter_inner, i=i)
+
+    iter_inner['epoch'].remove(-1)
+    iter_inner['global_steps'].remove(-1)
+    iter_outer[int(i)] = iter_inner  # keep i here!
+
+# statistics (best metric for each iteration - mean / median / min / max / stdev / var)
+outputfile = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+with open(f'{dataset}_{outputfile}.json', 'w') as f:
+    json.dump(iter_outer, f, indent=2)
+util.sum_best_dev(f'{dataset}_{outputfile}')
+util.add_statistics(f'{dataset}_{outputfile}.csv')
+
+endtime = time.time()  
+
+elapsed = endtime - starttime
+logging.info(f"Time elapsed: {util.hours_minutes_seconds(elapsed)}")
